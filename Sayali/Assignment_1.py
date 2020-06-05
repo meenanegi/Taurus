362d071c7b6cd33efbbc7262338c2764e682cf42
Sayali811 <sayalisatam81995@gmail.com>
2020-06-05 17:05:17 +0530

    added Sayali folder

---
+++ Sayali/Assignment_1.py
@@ -0,0 +1,17 @@
+# question 1
+# studentName="Yogesh"
+# age=19
+# newStudent=True
+#question 2
+# name=input("Enter your name:")
+# favFood=input("Enter your favorite food:")
+# print(name+" likes "+favFood)
+#Question 3
+# degreeCel=input("Enter the temperature in Degree celcius: ")
+# degcel=int(degreeCel)
+# degFah=(degcel*9/5)+32
+# print(degreeCel+" temp in fahrenheit is : "+str(degFah))
+#Question 4
+firstName=input(" ENTER FIRST NAME: ")
+lastName=input("ENTER LAST NAME: ")
+print(firstName.capitalize()+" "+lastName.capitalize())
